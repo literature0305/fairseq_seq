1ef673ab654a2246462c63d508204533bd2101df
literature0305 <31749989+literature0305@users.noreply.github.com>
2024-02-19 17:03:48 +0900

    Update 3_decode_viterbi.sh

--- 3_decode_viterbi.sh
+++ 3_decode_viterbi.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
-stage=7
+stage=-1
 stop_stage=100
 
 combined_data_dir=/home/Workspace/fairseq/data/combined_data
 # subset=dev_clean
 subsets="test_clean test_other dev_clean dev_other"
-dir_checkpoint=pretrained_models/errlog050_best.pt #errlog003.pt # /home/Workspace/fairseq/outputs/2023-02-16/01-15-13/errlog003_kd/checkpoint_best.pt
+dir_checkpoint=outputs/2024-02-14/12-43-04/checkpoints/checkpoint_best.pt # pretrained_models/errlog050_best.pt #errlog003.pt # /home/Workspace/fairseq/outputs/2023-02-16/01-15-13/errlog003_kd/checkpoint_best.pt
 # dir_checkpoint=outputs/2023-02-23/16-39-01/errlog005_sdt_kd/checkpoint_best.pt
 results_path=/home/Workspace/fairseq/decode_results
 SHELL_PATH=`pwd -P`
 echo $SHELL_PATH
 
-if [ ${stage} -le 7 ] && [ ${stop_stage} -ge 7 ]; then
-    echo "stage 7: do decode (viterbi)"
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "stage 1: prepare decoding"
+    cat data/test-clean/wav_dir.tsv > $combined_data_dir/test_clean.tsv
+    cat data/test-clean/labels.ltr > $combined_data_dir/test_clean.ltr
+    cat data/test-clean/labels.wrd > $combined_data_dir/test_clean.wrd
+
+    cat data/test-other/wav_dir.tsv > $combined_data_dir/test_other.tsv
+    cat data/test-other/labels.ltr > $combined_data_dir/test_other.ltr
+    cat data/test-other/labels.wrd > $combined_data_dir/test_other.wrd
+fi
+
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+    echo "stage 2: do decode (viterbi)"
     for subset in $subsets; do
         echo "Start decode $subset"
         python3 /home/Workspace/fairseq/examples/speech_recognition/infer.py $combined_data_dir --task audio_finetuning \
